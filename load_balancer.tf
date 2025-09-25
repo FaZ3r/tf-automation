0d417ccc767a7f945d382b0be12f756277dbecfa
FaZ3r <74310446+FaZ3r@users.noreply.github.com>
2025-09-25 12:41:25 +0300

    added listener to load balancer

--- load_balancer.tf
+++ load_balancer.tf
@@ -11,3 +11,14 @@ resource "aws_lb" "demo_alb" {
   subnets            = [aws_subnet.MySubnet1.id, aws_subnet.MySubnet2.id]
   security_groups    = [aws_security_group.new_sec_group.id]
 }
+
+resource "aws_lb_listener" "demo_listener" {
+  load_balancer_arn = aws_lb.demo_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.demo_tg.arn
+  }
+}
