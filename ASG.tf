d8ab82b9045f9beda270976df45d4cc131ccdbe0
FaZ3r <74310446+FaZ3r@users.noreply.github.com>
2025-10-13 18:06:58 +0300

    Updated more resource names

--- ASG.tf
+++ ASG.tf
@@ -2,7 +2,7 @@ resource "aws_autoscaling_group" "demo_asg" {
   desired_capacity    = 1
   max_size            = 2
   min_size            = 1
-  vpc_zone_identifier = [aws_subnet.MySubnet1.id, aws_subnet.MySubnet2.id]
+  vpc_zone_identifier = [aws_subnet.SubnetA.id, aws_subnet.SubnetB.id]
 
   launch_template {
     id     = aws_launch_template.demo_lt.id
