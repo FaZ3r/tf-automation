b6637a650bc9e24aee28f88576af57734a2202a1
Tudor Ureche <tudurech@emeal.nttdata.com>
2025-10-31 13:19:55 +0200

    removed slashes from shell script

--- instance_prep.sh
+++ instance_prep.sh
@@ -6,8 +6,4 @@ python3 -m ensurepip
 pip3 install --upgrade pip
 pip3 install randomtimestamp
 
-/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
-  -a fetch-config \
-  -m ec2 \
-  -c ssm:/CloudWatchAgentTudor/Config \
-  -s
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:/CloudWatchAgentTudor/Config -s
