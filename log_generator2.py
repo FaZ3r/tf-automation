aeb85ada3203f7aa2282f0eec7a36e24aa562307
FaZ3r <74310446+FaZ3r@users.noreply.github.com>
2025-11-04 14:40:11 +0200

    modified timestamp inside py script so it conforms to aws timestamp

--- log_generator2.py
+++ log_generator2.py
@@ -2,60 +2,55 @@ import random
 import randomtimestamp
 from datetime import datetime
 
+def random_log_datetime():
+    """Generate a random datetime within the range."""
+    return randomtimestamp.random_date(start=datetime(2025, 1, 1), end=datetime.now())
 
-def random_log_date():
-    return randomtimestamp.random_date(start=datetime(2025,1,1), end=datetime.now())
-
-def random_log_time():
-    return randomtimestamp.random_time()
+def aws_log_timestamp(dt=None):
+    """Return AWS-style timestamp string."""
+    if dt is None:
+        dt = datetime.utcnow()
+    return dt.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"  
 
 def random_log_type():
     return random.choice(['INFO', 'DEBUG', 'ERROR'])
 
-
 def random_app_log():
     return random.choice(['BackendApp', 'FrontendApp', 'API', 'SYSTEM'])
-
 
 def random_ms():
     return random.randrange(10, 30)
 
-
 def random_success_log_message_running(app):
-    return '{} has started running... \n'.format(app)
-
+    return f"{app} has started running..."
 
 def random_success_log_message_ms(app, ms):
-    return '{} has ran successfully in {}ms'.format(app, ms)
-
+    return f"{app} has ran successfully in {ms}ms"
 
 def random_fail_log_message():
-    return '{} has failed after {}ms. Retrying... '.format(random_app_log(), random_ms())
-
+    return f"{random_app_log()} has failed after {random_ms()}ms. Retrying..."
 
 def random_debug_log_message():
-    return '{} is still running, please wait... '.format(random_app_log())
+    return f"{random_app_log()} is still running, please wait..."
 
 
 logs = []
-for x in range(10):
+for _ in range(10):
     log_type = random_log_type()
-    if (log_type == 'ERROR'):
-        logs.append("{} {} - [{}] - {}\n".format(random_log_date(), random_log_time(), log_type, random_fail_log_message()))
-    elif (log_type == 'DEBUG'):
-        logs.append("{} {} - [{}] - {}\n".format(random_log_date(),random_log_time(), log_type, random_debug_log_message()))
+    log_dt = random_log_datetime()
+    log_ts = aws_log_timestamp(log_dt)
+
+    if log_type == 'ERROR':
+        logs.append(f"{log_ts} - [{log_type}] - {random_fail_log_message()}")
+    elif log_type == 'DEBUG':
+        logs.append(f"{log_ts} - [{log_type}] - {random_debug_log_message()}")
     else:
-        log_time = random_log_time()
-        log_date= random_log_date()
         app = random_app_log()
-        logs.append("{} {} - [{}] - {}".format(log_date, log_time, log_type, random_success_log_message_running(app)))
-        logs.append("{} {} - [{}] - {}\n".format(log_date,log_time, log_type, random_success_log_message_ms(app, random_ms())))
+        logs.append(f"{log_ts} - [{log_type}] - {random_success_log_message_running(app)}")
+        logs.append(f"{log_ts} - [{log_type}] - {random_success_log_message_ms(app, random_ms())}")
 
-def parse_timestamp(ts_str):
-    return datetime.strptime(ts_str[:19], "%Y-%m-%d %H:%M:%S")
 
-logs.sort(key=parse_timestamp)
-
+logs.sort()
 
 with open('/home/ec2-user/logs.txt', 'a') as f:
     for entry in logs:
