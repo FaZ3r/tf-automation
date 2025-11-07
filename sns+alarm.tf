9749418e047d61c4d2d9538ff7512877bb401522
Tudor Ureche <tudurech@emeal.nttdata.com>
2025-11-07 14:04:53 +0200

    modified the alarm resource to have correct syntax and trigger sns

--- sns+alarm.tf
+++ sns+alarm.tf
@@ -14,7 +14,7 @@ resource "aws_cloudwatch_log_metric_filter" "error_filter"{
     log_group_name= aws_cloudwatch_log_group.log_monitor_tudor.name
 
     metric_transformation {
-        name = "EventCount"
+        name = "EventCountTudor"
         namespace= "tudor_namespace"
         value="1"
     }
@@ -23,8 +23,15 @@ resource "aws_cloudwatch_log_metric_filter" "error_filter"{
 resource "aws_cloudwatch_metric_alarm" "ec2-log-alarm"{
     alarm_name = "ec2-log-tudor-alarm"
     comparison_operator = "GreaterThanThreshold"
-    evaluation_periods = 2
+    evaluation_periods = 1
+    
+    statistic = "Sum"
+    period = 60
+    threshold = 0
 
-    metric_name= aws_cloudwatch_log_metric_filter.error_filter.name
-    alarm_description ="this alarm monitors the occurences of errors in my log stream"
+    metric_name= "EventCountTudor"
+    namespace = "tudor_namespace"
+
+    alarm_description ="Alarm when 'ERROR' occurs in logs"
+    alarm_actions = aws_sns_topic.tudor_alarm.arn
 }
